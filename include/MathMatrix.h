6aadfdeb708d613ff3ea210bae70b249d4713d31
MATOO-Dev <fagoldob@gmail.com>
2023-05-18 14:14:49 +0200

    Added solutions for Tasks 4-6

--- include/MathMatrix.h
+++ include/MathMatrix.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include "MathVec.h"
 
 //row is x axis
 //column is y axis
@@ -13,16 +14,24 @@ private:
     T nullElement;
 public:
     MathMatrix(T defaultElement);
+	MathMatrix(const MathMatrix& source);
+    MathMatrix(MathMatrix&& source);
     ~MathMatrix();
     T get(unsigned int row, unsigned int col) const;
     void set(unsigned int row, unsigned int col, T value);
     T getNullElement();
-    T& operator[](unsigned int);
+    MathVec<T, colDim> getColumn(int index) const;
+    MathVec<T, rowDim> getRow(int index) const;
+	MathMatrix<T, rowDim, colDim>& operator=(const MathMatrix<T, rowDim, colDim>& source);
+	MathMatrix<T, rowDim, colDim>& operator=(MathMatrix<T, rowDim, colDim>&& source);
 };
 
 template<class T, unsigned int rowDim, unsigned int colDim>
 inline MathMatrix<T, rowDim, colDim>::MathMatrix(T defaultElement)
 {
+    if(colDim == 0 || rowDim == 0)
+        throw new logic_error("Error: matrix dimension must be larger than 0");
+
     //create container array
     data = new T*[rowDim];
 
@@ -40,8 +49,6 @@ inline MathMatrix<T, rowDim, colDim>::MathMatrix(T defaultElement)
         {
             //set value to default element
             data[x][y] = nullElement;
-            //debug
-            //std::cout << x << "," << y << " = " << data[x][y] << std::endl;
         }
     }
 }
@@ -49,6 +56,11 @@ inline MathMatrix<T, rowDim, colDim>::MathMatrix(T defaultElement)
 template<class T, unsigned int rowDim, unsigned int colDim>
 inline MathMatrix<T, rowDim, colDim>::~MathMatrix()
 {
+    //if data is nullptr, cancel
+    //this is used so the loop doesnt get called after move operations
+    if(data == nullptr)
+        return;
+
     //delete nested arrays
     for(int x = 0; x < rowDim; x++)
         delete[] data[x];
@@ -60,12 +72,16 @@ inline MathMatrix<T, rowDim, colDim>::~MathMatrix()
 template<class T, unsigned int rowDim, unsigned int colDim>
 inline T MathMatrix<T, rowDim, colDim>::get(unsigned int row, unsigned int col) const
 {
+    if(row >= rowDim || col >= colDim)
+        throw logic_error("Error: Matrix out of bounds for get()");
     return data[row][col];
 }
 
 template<class T, unsigned int rowDim, unsigned int colDim>
 inline void MathMatrix<T, rowDim, colDim>::set(unsigned int row, unsigned int col, T value)
 {
+    if(row >= rowDim || col >= colDim)
+        throw logic_error("Error: Matrix out of bounds for set()");
     data[row][col] = value;
 }
 
@@ -74,29 +90,6 @@ inline T MathMatrix<T, rowDim, colDim>::getNullElement()
 {
     return nullElement;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 template<class T, unsigned int rowDim, unsigned int colDim>
 inline std::string to_string(const MathMatrix<T, rowDim, colDim>& m) 
@@ -140,7 +133,97 @@ inline std::string to_string(const MathMatrix<T, rowDim, colDim>& m)
     return result;
 }
 
+template<class T, unsigned int rowDim, unsigned int colDim>
+inline MathVec<T, colDim> MathMatrix<T, rowDim, colDim>::getColumn(int index) const
+{
+    if(index >= rowDim)
+        throw logic_error("Error: cannot get column, specified index is larger than amount of columns");
 
+    MathVec<T, colDim> column = MathVec<T, colDim>(0);
+
+    for(int y = 0; y < colDim; y++)
+        column[y] = get(index, y);
+
+    return column;
+}
+
+template<class T, unsigned int rowDim, unsigned int colDim>
+inline MathVec<T, rowDim> MathMatrix<T, rowDim, colDim>::getRow(int index) const
+{
+    if(index >= colDim)
+        throw logic_error("Error: cannot get row, specified index is larger than amount of rows");
+
+    MathVec<T, rowDim> row = MathVec<T, rowDim>(0);
+
+    for(int x = 0; x < colDim; x++)
+        row[x] = get(x, index);
+
+    return row;
+}
+
+template<class T, unsigned int rowDim, unsigned int colDim>
+inline MathMatrix<T, rowDim, colDim>::MathMatrix(const MathMatrix& source)
+{
+    //copy null element
+    nullElement = source.nullElement;
+
+    data = new T*[rowDim];
+    //iterate over container array
+    for(int x = 0; x < rowDim; x++)
+    {
+        //fill each container element with a new nested array
+        data[x] = new T[colDim];
+
+        //iterate over nested array
+        for(int y = 0; y < colDim; y++)
+        {
+            //copy value from source element
+            data[x][y] = source.get(x, y);
+        }
+    }
+}
+
+template<class T, unsigned int rowDim, unsigned int colDim>
+inline MathMatrix<T, rowDim, colDim>::MathMatrix(MathMatrix&& source)
+{
+    nullElement = source.nullElement;
+    data = source.data;
+    source.data = nullptr;
+    source.nullElement = 0;
+}
+
+template<class T, unsigned int rowDim, unsigned int colDim>
+inline MathMatrix<T, rowDim, colDim>& MathMatrix<T, rowDim, colDim>::operator=(const MathMatrix<T, rowDim, colDim>& source)
+{
+    //copy null element
+    nullElement = source.nullElement;
+
+    data = new T*[rowDim];
+    //iterate over container array
+    for(int x = 0; x < rowDim; x++)
+    {
+        //fill each container element with a new nested array
+        data[x] = new T[colDim];
+
+        //iterate over nested array
+        for(int y = 0; y < colDim; y++)
+        {
+            //copy value from source element
+            data[x][y] = source.get(x, y);
+        }
+    }
+    return *this;
+}
+
+template<class T, unsigned int rowDim, unsigned int colDim>
+inline MathMatrix<T, rowDim, colDim>& MathMatrix<T, rowDim, colDim>::operator=(MathMatrix<T, rowDim, colDim>&& source)
+{
+    nullElement = source.nullElement;
+    data = source.data;
+    source.data = nullptr;
+    source.nullElement = 0;
+    return *this;
+}
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
+#include <string>
 #include "include/MathVec.h"
 #include "include/MathMatrix.h"
 
 int main()
 {
-    //test memory leak for destructor
-    MathMatrix<int, 9, 9> myMatrix = MathMatrix<int, 9, 9>(0);
+    //create a new matrix
+    std::cout << "Creating new instance myMatrix\n";
+    MathMatrix<int, 9, 5> myMatrix = MathMatrix<int, 9, 5>(0);
     std::cout << to_string(myMatrix);
 
-    myMatrix.set(4, 4, 6);
-
+    //setting a value
+    std::cout << "Changing a value in myMatrix\n";
+    myMatrix.set(4, 2, 6);
     std::cout << to_string(myMatrix);
+
+    //getting individual rows/columns
+    std::cout << "Getting individual row/column of myMatrix\n";
+    std::cout << to_string(myMatrix.getColumn(4)) << std::endl;
+    std::cout << to_string(myMatrix.getRow(2)) << std::endl << std::endl;
+
+    //testing copy constructor
+    std::cout << "Giving yourMatrix a copy of myMatrix\n";
+    MathMatrix<int, 9, 5> yourMatrix = MathMatrix<int, 9, 5>(myMatrix);
+    std::cout << to_string(yourMatrix);
+
+    //testing move constructor
+    std::cout << "Starting a communist revolution (moving myMatrix to ourMatrix)\n";
+    MathMatrix<int, 9, 5> ourMatrix = std::move(myMatrix);
+    std::cout << to_string(ourMatrix);
+
+    //moving ourMatrix back to myMatrix
+    std::cout << "Fall of the soviet union, 26.12.1991, colorized (moving ourMatrix back to myMatrix)\n";
+    myMatrix = std::move(ourMatrix);
+    std::cout << to_string(myMatrix);
+
 }
